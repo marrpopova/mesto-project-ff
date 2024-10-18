3c18cdfafee10e00ef1f62edf99406f8e2b3b99d
Marina Popova <marina2000popova@yandex.ru>
2024-10-18 15:06:44 +0300

    исправила функцию удаления и др

--- scripts/index.js
+++ scripts/index.js
@@ -5,22 +5,25 @@ const cardTemplate=document.querySelector('#card-template').content;
 const placesList=document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(name, link, deleteCard) {
+function createCard(object, index, deleteCard) {
     const cardElement=cardTemplate.querySelector('.places__item').cloneNode(true);
-    cardElement.querySelector('.card__image').src=link;
-    cardElement.querySelector('.card__image').alt=name;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    cardElement.querySelector('.card__title').textContent=name;
+    const cardImage=cardElement.querySelector('.card__image');
+    cardImage.src=object[index].link;
+    cardImage.alt=object[index].name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+    cardElement.querySelector('.card__title').textContent=object[index].name;
     return cardElement
 };
 
 // @todo: Функция удаления карточки
-function deleteCard(event) {
-    event.target.parentElement.remove();
+function deleteCard(card) {
+    card.remove();
 };
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach((element)=> {
-    const card=createCard(element.name, element.link, deleteCard);
+initialCards.forEach((element, index)=> {
+    const card=createCard(initialCards, index, deleteCard);
     placesList.append(card);
 });
